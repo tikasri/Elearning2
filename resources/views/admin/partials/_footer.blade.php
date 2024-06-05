ef8a0c9d5f0e9016bc84624c56f240eea1d09e72
tikasri <sariatika086@gmail.com>
2024-06-05 02:54:54 -0700

    praktikum 11 dsn 12

--- resources/views/admin/partials/_footer.blade.php
+++ resources/views/admin/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>Afifah Suryani 2024</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>Atikah Sari 2024</span></strong>. All Rights Reserved
     </div>
 </footer><!-- End Footer -->

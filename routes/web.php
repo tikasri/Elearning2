121d113ed14c2ef146c884f70fcfa2b44d642b32
tikasri <sariatika086@gmail.com>
2024-06-11 23:51:33 -0700

    praktikum 11 12 13

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,38 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StudentController;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 
 
 
 // routing profil
 
-Route::get('admin/dashboard', [DashboardController::class, 'index']);
 
-Route::get('admin/student', [StudentController::class, 'index']);
+
+
+// Route untuk menampilkan form tambah student
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('admin/student/create', [StudentController::class, 'create']);
+    Route::get('admin/student', [StudentController::class, 'index']);
 
 Route::get('admin/course', [CourseController::class, 'index']);
 
-// Route untuk menampilkan form tambah student
-Route::get('admin/student/create', [StudentController::class, 'create']);
 
 // ROute untuk mengirim data student baru
 Route::post('admin/student/store', [StudentController::class, 'store']);
@@ -34,3 +45,10 @@ Route::put('admin/student/update/{id}', [StudentController::class, 'update']);
 
 // Route untuk menghapus student
 Route::delete('admin/student/delete/{id}', [StudentController::class, 'destroy']);
+    Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
